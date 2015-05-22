6122137df27bc5303da291aa78b11fc5b45024bf
Maciej Kubiak <maciej.kubiak.net@gmail.com>
2015-05-22 12:12:23 +0200

    Enlarge sections height. Logo replace. Absolute scroll position. Adjusting animation

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,26 +1,21 @@
 //= require_tree .
 
 $(function() {
-  var team = $('.team')
-  var teamScrollPosition = $(document).height() - team.height() + 200;
-
-  function hideTarget(event) {
-    var $scroll = $(window).scrollTop(),
-        $target = $(event.data.target);
-
-    if ($scroll >= event.data.position) {
-        $target.addClass("hide");
-    } else {
-        $target.removeClass("hide");
-    }
-  }
-
   function smoothScroll(event){
-    var offset = event.data.offset
+    var offset = event.data.offset,
+        value = null
+
+    event.preventDefault();
+
+    if (event.data.value > 0) {
+      value = event.data.value
+    } else {
+      value = $($(this).attr('href')).offset().top
+    }
+
     $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top + offset
+        scrollTop: value
     }, 700);
-    return false;
   };
 
   if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
@@ -31,12 +26,11 @@ $(function() {
     $('[name=location]').on('click', {offset: 0}, smoothScroll);
     $('[name=home]').on('click', {offset: -100}, smoothScroll);
   } else {
-    $('[name=home]').on('click', {offset: 0}, smoothScroll);
-    $('[name=team]').on('click', {offset: 500}, smoothScroll);
-    $('[name=about]').on('click', {offset: 0}, smoothScroll);
-    $('[name=contact]').on('click', {offset: 0}, smoothScroll);
-    $('[name=location]').on('click', {offset: -50}, smoothScroll);
-    skrollr.init({forceHeight: false});
-    $(window).on('scroll', {position: teamScrollPosition, target: '.team'}, hideTarget);
+    skrollr.init({forceHeight: false, render: function(data) { console.log(data.curTop) }  });
+    $('[name=home]').on('click', {value: 0}, smoothScroll);
+    $('[name=team]').on('click', {value: 2100}, smoothScroll);
+    $('[name=about]').on('click', {value: 1300}, smoothScroll);
+    $('[name=contact]').on('click', {value: 3250}, smoothScroll);
+    $('[name=location]').on('click', {value: 4400}, smoothScroll);
   }
 });
